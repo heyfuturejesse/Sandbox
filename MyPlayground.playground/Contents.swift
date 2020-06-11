7939c71c9757ebeeef8a78d71b9572cc47faea99
heyfuturejesse <jesse@heyfuturejesse.com>
2020-06-11 22:35:51 +1000

    Further review items added. This time around Structs.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -115,3 +115,115 @@ let french = translator("Hello")
 
 print(french)
 */
+
+
+
+
+
+// Structs
+// Computed properties
+/*
+struct Sport {
+    var name: String
+    var isOlympicSport: Bool
+    
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic Sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let afl = Sport(name: "Aussie Rules Football", isOlympicSport: false)
+print(afl.olympicStatus)
+*/
+
+
+
+
+//Property observers
+/*
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+    
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+*/
+
+/*
+struct BankAccount {
+    var name: String
+    var isMillionnaire = false
+    var balance: Int {
+        didSet {
+            if balance > 1_000_000 {
+                isMillionnaire = true
+                print("You got some cash man, look at how much you got: \(balance)")
+            } else {
+                isMillionnaire = false
+                print("You got some cash to make man, look at how much you got: \(balance)")
+            }
+        }
+    }
+}
+
+var money = BankAccount(name: "comm bank", balance: 1_000_000)
+money.balance = 100
+money.balance = 1_000_001
+*/
+
+
+
+//Methods
+/*
+struct Fruits {
+    var cropNumber: Int
+    
+    func collectTithe() -> Int {
+        return cropNumber * 1000
+    }
+}
+
+let corn = Fruits(cropNumber: 9_000_000)
+corn.collectTithe()
+*/
+
+
+
+
+/*
+struct User {
+    var name: String
+    var street: String
+    var city: String
+    var postalCode: String
+    func printAddress() -> String {
+        return """
+        \(name)
+        \(street)
+        \(city)
+        \(postalCode)
+        """
+    }
+}
+
+let Banana = User(name: " John Freeman \n",
+                  street: "89 Example Street\n",
+                  city: "Point Vernon\n",
+                  postalCode: "4101\n")
+print(Banana.name,
+      Banana.street,
+      Banana.city,
+      Banana.postalCode)
+*/
